fd224e09c5758ff1e3a5c59f341bbea0d425437a
Jakob Fix <jakob@jfix.com>
2018-04-06 21:32:02 +0200

    🐛  Fix bug on Windows due to specific path separator. Fix #14.

--- plugins/_plugin-loader/loader.js
+++ plugins/_plugin-loader/loader.js
@@ -5,16 +5,16 @@
 */
 
 const { lstatSync, readdirSync, readFileSync } = require('fs')
-const { join, resolve } = require('path')
+const path = require('path')
 const Store = require('electron-store')
 const store = new Store()
 
 const isDirectory = source => lstatSync(source).isDirectory()
 const getDirectories = source =>
-  readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+  readdirSync(source).map(name => path.join(source, name)).filter(isDirectory)
 
 const readPluginConf = (dir) => {
-  return JSON.parse(readFileSync(resolve(dir, 'plugin.json'), 'utf8'))
+  return JSON.parse(readFileSync(path.resolve(dir, 'plugin.json'), 'utf8'))
 }
 const PluginLoader = function () {}
 let _plugins = {}
@@ -41,7 +41,7 @@ PluginLoader.prototype.loadAll = (pluginDirectory) => {
   _pluginDir = pluginDirectory
   const dirs = getDirectories(pluginDirectory)
   dirs.forEach((d) => {
-    const pluginDirName = d.split('/').slice(-1)[0]
+    const pluginDirName = d.split(path.sep).slice(-1)[0]
     // ignore private plugins that start with _
     if (pluginDirName.indexOf('_') !== 0) {
       _plugins[pluginDirName] = readPluginConf(d)
@@ -57,7 +57,7 @@ PluginLoader.prototype.deactivatePlugin = (pluginId) => {
 PluginLoader.prototype.activatePlugin = (pluginId) => {
   const plugin = _plugins[pluginId]
   if (plugin.module) {
-    require(resolve(_pluginDir, pluginId, plugin.module))()
+    require(path.resolve(_pluginDir, pluginId, plugin.module))()
     store.set(`plugins.lastUsed.${pluginId}`, new Date())
   }
 }
